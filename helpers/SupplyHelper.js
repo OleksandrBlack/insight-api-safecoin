452a0de797eb94656d1d91cc36af8d9c2a5e5abf
OleksandrBlack <aleksandr.chornuy@gmail.com>
2019-02-01 22:23:52 +0200

    up version

---
+++ helpers/SupplyHelper.js
@@ -0,0 +1,13 @@
+var BigNumber = require('bignumber.js');
+
+module.exports = {
+    /**
+     *
+     * @param {Number} height
+     * @return {BigNumber}
+     */
+    getTotalSupplyByHeight: function (height) {
+        return (new BigNumber(12582500)).plus((height) * 150);
+    }
+
+};
